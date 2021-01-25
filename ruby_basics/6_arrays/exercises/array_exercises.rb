128afb6dbe04b3a98fcf87eefaa3371366f6c931
Prabhjot <pjsingh1359@gmail.com>
2021-01-25 13:30:00 -0800

    changed methods to use array methods

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -1,26 +1,16 @@
 def nil_array(number)
   # return an array containing the number of nil values
-  my_arr = []
-  while number > 0
-    my_arr.push(nil)
-    number -= 1
-  end
-  return my_arr
+ Array.new(number, nil)
 end
 
 def nested_array(number)
   # return an array containing the number of empty arrays
-  my_arr = []
-  while number > 0
-    my_arr.push([])
-    number -= 1
-  end
-  return my_arr
+ Array.new(number, [])
 end
 
 def first_element(array)
   # return the first element of the array
-  return array[0]
+  return array.first
 end
 
 def third_element(array)
@@ -30,62 +20,58 @@ end
 
 def last_three_elements(array)
   # return the last 3 elements of the array
-  if array.length < 3
-    return array
-  end
-  return array[-3..-1]
+  array.last(3);
 end
 
 def add_element(array)
   # add an element (of any value) to the array
-  return array << 42
+  p array.push(42)
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
   array.pop
-  return array
   # Step 2: return the array (because Step 1 returns the value of the element removed)
-  array
+  p array
 end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
   array.shift(3);
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
-  return array
+  p array
 end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
-  return original + additional
+  p original.concat(additional)
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
-  return original - comparison
+  p original - comparison
 
 end
 
 def empty_array?(array)
-  return array.empty?
+  array.empty?
 end
 
 def reverse(array)
-  return array.reverse
+  array.reverse
 end
 
 def array_length(array)
   # return the length of the array
-  return array.length
+  array.size
 end
 
 def include?(array, value)
   # return true if the array includes the value
-  return array.include?(value)
+  array.include?(value)
 end
 
 def join(array, separator)
   # return the result of joining the array with the separator
-  return array.join(separator)
+  array.join(separator)
 end
