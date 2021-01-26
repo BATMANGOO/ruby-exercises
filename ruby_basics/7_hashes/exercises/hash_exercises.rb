c777c7fad9c788b8df5df4cc082eaba2c4c8f267
Prabhjot <pjsingh1359@gmail.com>
2021-01-26 15:35:44 -0800

    changed logic to be more method focused

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -10,42 +10,39 @@ end
 
 def favorite_color(favorite_list)
   # return the value of the color key
-  return favorite_list[:color]
+  favorite_list[:color]
 end
 
 def favorite_number(favorite_list)
   # return the value of the number key or 42 if the key is not found
-  if !favorite_list[:number]
-    return 42
-  end
-  return favorite_list[:number]
+  favorite_list.fetch(:number, 42)
 end
 
 def update_favorite_movie(favorite_list, movie)
   # Step 1: add/update the key of movie (as a symbol)
   favorite_list[:movie] = movie
   # Step 2: return the hash (because Step 1 returns the value of the movie key)
-  return favorite_list
+  p favorite_list
 end
 
 def remove_favorite_number(favorite_list)
   # Step 1: delete the number data
   favorite_list.delete(:number)
   # Step 2: return the hash (because Step 1 returns the value of the number key)
-  return favorite_list
+  p favorite_list
 end
 
 def favorite_categories(favorite_list)
   # return the keys of favorite_list
-  return favorite_list.keys
+  p favorite_list.keys
 end
 
 def favorite_items(favorite_list)
   # return the values of favorite_list
-  return favorite_list.values
+  p favorite_list.values
 end
 
 def merge_favorites(original_list, additional_list)
   # merge the two hashes: original_list and additional_list
-  return original_list.merge(additional_list)
+  p original_list.merge(additional_list)
 end
